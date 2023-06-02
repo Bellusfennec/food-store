f617bbf20af244cad4691b84e107e76c5725cfad
bellusfennec <bellusfennec@gmail.com>
2023-06-02 16:15:14 +0300

    refactor:

--- src/pages/product/components/Tags.jsx
+++ src/pages/product/components/Tags.jsx
@@ -4,109 +4,74 @@ import style from "./Tags.module.scss";
 import { Link } from "react-router-dom";
 import { IconButton } from "../../../common/components/ui/form";
 import { IoChevronBackOutline } from "react-icons/io5";
-import { log } from "util";
 
 const Tags = (props) => {
   // const { options } = props;
   const initOptions = props.options ? props.options : [];
   const [options, setOtpions] = useState(initOptions);
   const [scroll, setScroll] = useState(0);
-
   let ref = useRef(null);
-  const [arrow, setArrow] = useState({
-    left: true,
-    right: true,
+  const [state, setState] = useState({
+    isScrolling: false,
+    clientX: 0,
+    scrollX: 0,
   });
 
-  // useEffect(() => {
-  //   console.log("ref", ref, ref.current.scrollLeft);
-  // }, [ref.current]);
-  // const [state, setState] = useState({
-  //   isScrolling: false,
-  //   clientX: 0,
-  //   scrollX: 0,
-  // });
+  console.log("state", state);
 
-  // console.log(state);
+  useEffect(() => {
+    console.log("ref", ref, ref.current.scrollLeft);
+  }, [ref.current]);
 
-  // useEffect(() => {
-  //   const element = ref.current;
-  //   if (element) {
-  //     const onWeel = (event) => {
-  //       event.preventDefault();
-  //       element.scrollTo({
-  //         left: element.scrollLeft + event.deltaY * 4,
-  //         behavior: "smooth",
-  //       });
-  //     };
-  //     element.addEventListener("wheel", onWeel);
-  //     return () => element.removeEventListener("wheel", onWeel);
-  //   }
-  // }, []);
+  const handlerOnMouseDown = (event) => {
+    if (ref && ref.current && !ref.current.contains(event.target)) {
+      return;
+    }
+    event.preventDefault();
+    console.log("handlerOnMouseDown");
+    setState({ ...state, isScrolling: true, clientX: event.clientX });
+  };
 
-  // const handlerOnMouseDown = (event) => {
-  //   console.log(ref.current.contains(event.target));
-  //   if (ref && ref.current && !ref.current.contains(event.target)) {
-  //     return;
-  //   }
-  //   event.preventDefault();
-  //   console.log("handlerOnMouseDown");
-  //   setState({ ...state, isScrolling: true, clientX: event.clientX });
-  // };
+  const handlerOnMouseUp = (event) => {
+    if (state.isScrolling) {
+      event.preventDefault();
+      console.log("handlerOnMouseUp");
+      setState({ ...state, isScrolling: false });
+    }
+  };
 
-  // const handlerOnMouseUp = (event) => {
-  //   if (state.isScrolling) {
-  //     event.preventDefault();
-  //     console.log("handlerOnMouseUp");
-  //     setState({ ...state, isScrolling: false });
-  //   }
-  // };
+  const handlerOnMouseMove = (event) => {
+    if (ref && ref.current && !ref.current.contains(event.target)) {
+      console.log("handlerOnMouseMove return");
+      return;
+    }
+    event.preventDefault();
+    console.log("handlerOnMouseMove");
+    const { clientX, scrollX, isScrolling } = state;
 
-  // const handlerOnMouseMove = (event) => {
-  //   if (ref && ref.current && !ref.current.contains(event.target)) {
-  //     console.log("handlerOnMouseMove return");
-  //     return;
-  //   }
-  //   event.preventDefault();
-  //   console.log("handlerOnMouseMove");
-  //   const { clientX, scrollX, isScrolling } = state;
+    if (isScrolling) {
+      let newScrollX = scrollX + event.clientX - clientX;
+      let newClientX = event.clientX;
+      ref.current.scrollLeft = newScrollX;
+      ref.current.style.transform = `translate(${newScrollX}px)`;
 
-  //   if (isScrolling) {
-  //     let sX = scrollX + event.clientX - clientX;
-  //     let cX = event.clientX;
-  //     ref.current.scrollLeft = sX;
-
-  //     console.log("handlerOnMouseMove", "sX:", sX, "cX:", cX);
-  //     setState({
-  //       ...state,
-  //       scrollX: sX,
-  //       clientX: cX,
-  //     });
-  //   }
-  // };
-
-  const outNum = (toNumber, elem) => {
-    const time = 10000;
-    const step = 1;
-    let n = 0;
-    let t = Math.round(time / (toNumber / step));
-    let interval = setInterval(() => {
-      n = n + step;
-      if (n === toNumber) {
-        clearInterval(interval);
-      }
-      elem = n;
-    }, t);
+      setState({
+        ...state,
+        scrollX: newScrollX,
+        clientX: newClientX,
+      });
+    }
   };
 
   const handlerScrollX = (arrow) => {
+    const element = ref.current;
     /* Видимая ширина */
-    const clientWidth = ref.current.clientWidth;
+    const clientWidth = element.clientWidth;
     /* Общая ширина */
-    const scrollWidth = ref.current.scrollWidth;
+    const scrollWidth = element.scrollWidth;
     /* */
     const doubleScroll = clientWidth * 2;
-    const percentScroll = (clientWidth / 100) * 80;
+    const percentScroll = (clientWidth / 100) * 75;
     /* Ширина скрола */
     let scrollToLeft = scroll + percentScroll;
     let scrollToRight = scroll - percentScroll;
@@ -127,14 +92,56 @@ const Tags = (props) => {
       if (goStart) scrolled = 0;
     }
 
-    ref.current.style.transform = `translate(${scrolled}px)`;
+    element.style.transform = `translate(${scrolled}px)`;
     setScroll(scrolled);
   };
+
+  const handlerWheel = (event) => {
+    const element = ref.current;
+    if (element) {
+      /* Видимая ширина */
+      const clientWidth = element.clientWidth;
+      /* Общая ширина */
+      const scrollWidth = element.scrollWidth;
+      /* */
+      const doubleScroll = clientWidth * 2;
+      const percentScroll = (clientWidth / 100) * 75;
+      /* Ширина скрола */
+      let scrollToLeft = scroll + percentScroll;
+      let scrollToRight = scroll - percentScroll;
+      const lastScroll = (scrollWidth - clientWidth) * -1;
+      /* */
+      let scrolled;
+      if (event.deltaY < 0) {
+        const isNearStart = scroll > doubleScroll * -1;
+        scrolled = isNearStart ? 0 : scrollToLeft;
+        /* Если начало */
+        const goEnd = isNearStart && scroll === 0;
+        if (goEnd) scrolled = lastScroll;
+      } else if (event.deltaY > 0) {
+        const isNearEnd = Math.abs(scroll) > scrollWidth - doubleScroll;
+        scrolled = isNearEnd ? lastScroll : scrollToRight;
+        /* Если конец */
+        const goStart = isNearEnd && scroll === lastScroll;
+        if (goStart) scrolled = 0;
+      }
+      element.style.transform = `translate(${scrolled}px)`;
+      setScroll(scrolled);
+    }
+  };
+  useEffect(() => {
+    const element = ref.current;
+    if (element) {
+      const onWeel = (event) => event.preventDefault();
+      element.addEventListener("wheel", onWeel);
+      return () => element.removeEventListener("wheel", onWeel);
+    }
+  }, []);
 
   return (
     <div className={style.container}>
       <div className={style.arrow}>
-        <div className={style.rightBackground}>
+        <div className={style.leftBackground}>
           <IconButton
             className={style.lightArrow}
             type="button"
@@ -143,7 +150,7 @@ const Tags = (props) => {
             <IoChevronBackOutline />
           </IconButton>
         </div>
-        <div className={style.leftBackground}>
+        <div className={style.rightBackground}>
           <IconButton
             className={style.rightArrow}
             type="button"
@@ -157,12 +164,11 @@ const Tags = (props) => {
         <div
           className={style.scroll}
           ref={ref}
-          // onScroll={}
-          // onScroll={}
-          // onMouseDown={handlerOnMouseDown}
-          // onMouseUp={handlerOnMouseUp}
-          // onMouseMove={handlerOnMouseMove}
-          // onMouseLeave={handlerOnMouseUp}
+          onWheel={handlerWheel}
+          onMouseDown={handlerOnMouseDown}
+          onMouseUp={handlerOnMouseUp}
+          onMouseMove={handlerOnMouseMove}
+          onMouseLeave={handlerOnMouseUp}
         >
           {options &&
             options.map(({ name, uuid }) => (
